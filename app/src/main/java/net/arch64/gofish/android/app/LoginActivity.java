179e8266ab005af381174ca5605b331066488742
rory <rory.dudley@gmail.com>
2019-04-16 12:11:17 -0400

    fixing login crashing problem

--- app/src/main/java/net/arch64/gofish/android/app/LoginActivity.java
+++ app/src/main/java/net/arch64/gofish/android/app/LoginActivity.java
@@ -85,7 +85,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
 
-        @SuppressLint("WrongViewCast") Button mEmailSignInButton = (Button) findViewById(R.id.email);
+        @SuppressLint("WrongViewCast") Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
