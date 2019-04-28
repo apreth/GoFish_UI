71b62c04c6fda7e0b4507ea3bad910f2890fe2ec
rory <rory.dudley@gmail.com>
2019-04-28 14:07:57 -0400

    locale query functions added

--- app/src/main/java/net/arch64/gofish/android/app/ForumActivity.java
+++ app/src/main/java/net/arch64/gofish/android/app/ForumActivity.java
@@ -1,7 +1,20 @@
 package net.arch64.gofish.android.app;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.MeasureSpec;
@@ -9,19 +22,29 @@ import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.gofish.R;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class ForumActivity extends AppCompatActivity {
+    private LocationManager locMan;
+    private LocationListener locLst;
+    private TextView locTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forum);
+
+        locTextView = (TextView) findViewById(R.id.locTextView);
 
         ListView forumListView = (ListView) findViewById(R.id.forumList);
         setListViewHeightBasedOnChildren(forumListView);
@@ -47,6 +70,60 @@ public class ForumActivity extends AppCompatActivity {
 
         ListAdapter listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, forumList);
         forumListView.setAdapter(listAdapter);
+
+        locMan = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locLst = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                //Log.d("LOCATION", location.toString());
+                //Toast.makeText(getApplicationContext(), location.getLongitude() + " " + location.getLatitude(), Toast.LENGTH_LONG).show();
+                getLocale(location.getLatitude(), location.getLongitude());
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(settingsIntent);
+            }
+        };
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.INTERNET
+                }, 10);
+                return;
+            }
+        }
+        locMan.requestLocationUpdates("gps", 5000, 0, locLst);
+    }
+
+    public void getLocale(double lat, double lng) {
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(lat, lng, 1);
+            Address obj = addresses.get(0);
+            String loc = obj.getLocality() + ", " + obj.getAdminArea() + ", " + obj.getCountryCode();
+            //Log.d("LOCATION-STRING", loc);
+            //Toast.makeText(getApplicationContext(), loc, Toast.LENGTH_LONG).show();
+            if (!locTextView.getText().equals(loc)) {
+                locTextView.setText(loc);
+            }
+        } catch (IOException e) {
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
 
     /**** Method for Setting the Height of the ListView dynamically.
