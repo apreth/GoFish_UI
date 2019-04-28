0f6d8f0c9b298013074a4dd79409fdb950387e26
rory <rory.dudley@gmail.com>
2019-04-28 19:16:41 -0400

    fixing formatting and adding nav bar to write post activity

--- app/src/main/java/net/arch64/gofish/android/app/WritePostActivity.java
+++ app/src/main/java/net/arch64/gofish/android/app/WritePostActivity.java
@@ -1,8 +1,12 @@
 
 package net.arch64.gofish.android.app;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,11 +14,41 @@ import android.widget.EditText;
 import com.example.gofish.R;
 
 public class WritePostActivity extends AppCompatActivity {
+    private BottomNavigationView navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_post);
+
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.getMenu().getItem(1).setChecked(true);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                //Log.d("NAV_TITLE", menuItem.getTitle().toString());
+                Intent intent = null;
+                switch(menuItem.getTitle().toString()) {
+                    case "Forums":
+                        intent = new Intent(getApplicationContext(), ForumActivity.class);
+                        break;
+                    case "Make Forum Post":
+                        intent = new Intent(getApplicationContext(), WritePostActivity.class);
+                        break;
+                    case "Profile Page":
+                        intent = new Intent(getApplicationContext(), ProfileActivity.class);
+                        break;
+                    default:
+                        intent = new Intent(getApplicationContext(), ForumActivity.class);
+                        break;
+                }
+                menuItem.setChecked(true);
+                if (intent != null) {
+                    startActivity(intent);
+                }
+                return false;
+            }
+        });
 
         EditText postText = (EditText) findViewById(R.id.postEditText);
         String post = postText.getText().toString();
